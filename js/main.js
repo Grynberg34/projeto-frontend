86bae7d3443a2385d392fa9e49a2122bb4b50988
FranciscoGB <franciscogrynberg34@gmail.com>
2020-09-10 16:35:38 -0300

    Implements BEM methodology to the project

--- js/main.js
+++ js/main.js
@@ -1,71 +1,72 @@
 // Mostrar LP
 
-document.getElementById("cover").addEventListener("click", function mostrarLP(){
-    document.getElementById("lp").style.opacity = 1;
-    document.getElementById("lp").style.marginLeft = "244px";
-    document.getElementById("lp").style.marginTop = "183px";
+document.getElementsByClassName("vitrola__cover")[0].addEventListener("click", function mostrarLP(){
+    document.getElementsByClassName("vitrola__lp")[0].style.opacity = 1;
+    document.getElementsByClassName("vitrola__lp")[0].style.marginLeft = "244px";
+    document.getElementsByClassName("vitrola__lp")[0].style.marginTop = "183px";
 })
+
 
 // hastes
 
-document.getElementById("pausar").addEventListener("click", function pausar() {
-    document.getElementById("música").pause()
-    document.getElementById("lp-img").classList.remove("rotating")
-    document.getElementById("haste").style.marginTop = "329px"
-    document.getElementById("haste").style.marginLeft = "1041px"
-    document.getElementById("haste").style.transform = "none"
+document.getElementsByClassName("vitrola__música--pausar")[0].addEventListener("click", function pausar() {
+    document.getElementsByClassName("vitrola__música")[0].pause()
+    document.getElementsByClassName("vitrola__lp--img")[0].classList.remove("rotating")
+    document.getElementsByClassName("vitrola__haste")[0].style.marginTop = "329px"
+    document.getElementsByClassName("vitrola__haste")[0].style.marginLeft = "1041px"
+    document.getElementsByClassName("vitrola__haste")[0].style.transform = "none"
 
-    document.getElementById("pausar").style.display = "none"
-    document.getElementById("tocar").style.display = "block"
+    document.getElementsByClassName("vitrola__música--pausar")[0].style.display = "none"
+    document.getElementsByClassName("vitrola__música--tocar")[0].style.display = "block"
 })
 
-document.getElementById("tocar").addEventListener("click", function tocar() {
-    document.getElementById("haste").style.marginTop = "312px"
-    document.getElementById("haste").style.marginLeft = "1006px"
-    document.getElementById("haste").style.transform = "rotate(18deg)"
+document.getElementsByClassName("vitrola__música--tocar")[0].addEventListener("click", function tocar() {
+    document.getElementsByClassName("vitrola__haste")[0].style.marginTop = "312px"
+    document.getElementsByClassName("vitrola__haste")[0].style.marginLeft = "1006px"
+    document.getElementsByClassName("vitrola__haste")[0].style.transform = "rotate(18deg)"
 
-    document.getElementById("lp-img").classList.add("rotating")
+    document.getElementsByClassName("vitrola__lp--img")[0].classList.add("rotating")
 
 
-    document.getElementById("tocar").style.display = "none"
-    document.getElementById("pausar").style.display = "block"
+    document.getElementsByClassName("vitrola__música--tocar")[0].style.display = "none"
+    document.getElementsByClassName("vitrola__música--pausar")[0].style.display = "block"
 
     // Essas duas linhas são para evitar um problema no Safari, que só toca o áudio da segunda vez que é dado o comando de play
-    document.getElementById("música").play()
-    document.getElementById("música").pause()    
+    document.getElementsByClassName("vitrola__música")[0].play()
+    document.getElementsByClassName("vitrola__música")[0].pause()    
 
     setTimeout(function playing(){ 
-    document.getElementById("música").play() }, 1000);
+    document.getElementsByClassName("vitrola__música")[0].play() }, 1000);
 
-    document.getElementById("música").onended = function() {
-        document.getElementById("lp-img").classList.remove("rotating")
-        document.getElementById("haste").style.marginTop = "329px"
-        document.getElementById("haste").style.marginLeft = "1041px"
-        document.getElementById("haste").style.transform = "none"
-        document.getElementById("tocar").style.display = "block"
-        document.getElementById("pausar").style.display = "none"
+    document.getElementsByClassName("vitrola__música")[0].onended = function() {
+        document.getElementsByClassName("vitrola__lp--img")[0].classList.remove("rotating")
+        document.getElementsByClassName("vitrola__haste")[0].style.marginTop = "329px"
+        document.getElementsByClassName("vitrola__haste")[0].style.marginLeft = "1041px"
+        document.getElementsByClassName("vitrola__haste")[0].style.transform = "none"
+        document.getElementsByClassName("vitrola__música--tocar")[0].style.display = "block"
+        document.getElementsByClassName("vitrola__música--pausar")[0].style.display = "none"
     }
 })
 
 //Drag & Drop API
 
-document.getElementById("lp").addEventListener("dragstart", function dragstart_handler(ev) {
+document.getElementsByClassName("vitrola__lp")[0].addEventListener("dragstart", function dragstart_handler(ev) {
     ev.dataTransfer.setData("application/my-app", ev.target.id);
     ev.dataTransfer.dropEffect = "move";
-    document.getElementById("lp").style.opacity = 0
+    document.getElementsByClassName("vitrola__lp")[0].style.opacity = 0
     let img = new Image(); 
-    img.src = './img/lp.png'; 
+    img.src = './img/vitrola/lp.png'; 
     ev.dataTransfer.setDragImage(img, 100, 100);
 })
 
-document.getElementById("dropzone").addEventListener("dragover", function dragover_handler(ev) {
+document.getElementsByClassName("vitrola__dropzone")[0].addEventListener("dragover", function dragover_handler(ev) {
     ev.preventDefault();
     ev.dataTransfer.dropEffect = "move"
 })
 
-document.getElementById("dropzone").addEventListener("drop", function drop_handler(ev) {
+document.getElementsByClassName("vitrola__dropzone")[0].addEventListener("drop", function drop_handler(ev) {
     ev.preventDefault();
     const data = ev.dataTransfer.getData("application/my-app");
     ev.target.appendChild(document.getElementById(data));
-    document.getElementById("tocar").style.display = "block"
+    document.getElementsByClassName("vitrola__música--tocar")[0].style.display = "block"
 })
